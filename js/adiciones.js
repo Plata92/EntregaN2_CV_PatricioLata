9e5e75d94f48b7cba918111a069cd40777157cad
Patricio Lata <pato-albo92@hotmail.com>
2023-02-26 18:47:39 -0300

    Subir Proyecto

--- js/adiciones.js
+++ js/adiciones.js
@@ -1,3 +1,13 @@
-document.getElementsById('boton').onclick = function (){
-    document.getElementById('nombre').innerHTML="Ruben Raul Ramirez";
+document.getElementById('cambiarnombre').onclick = function () {
+    console.log("hola mundo");
+    document.getElementById("nombre").innerHTML = "Esteban Marquisio";
+}
+
+document.getElementById('cambiarImagen').onclick = function () {
+    console.log("probando funcion");
+    document.getElementById("myImage").src="ImagenCV2.jpg";
+}
+
+document.getElementById('btn_color').onclick = function () {
+    document.getElementById("fondo").style.background = "#FF0000";
 }
